dbdfac4dfa18c48cef2d6082040f990fbc64493c
Godbooer <1198712616@qq.com>
2026-04-30 12:01:31 +0800

    fix: 拆分main.py和app.py, 添加setup.py, 解决p4a入口解析IndexError

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='android-file-browser',
+    version='2.0.0',
+    packages=[],
+    install_requires=[
+        'kivy>=2.3.0',
+        'Pillow',
+    ],
+)
